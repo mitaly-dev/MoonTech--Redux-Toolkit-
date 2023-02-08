daee6fbdc1e5545f1df1feca970e2aeaca2616e7
mitaly-dev <mitalyy.dev@gmail.com>
2023-02-08 11:24:49 -0700

    updated

--- src/service/store.js
+++ src/service/store.js
@@ -1,12 +1,14 @@
-import {configureStore} from '@reduxjs/toolkit'
+import {configureStore, getDefaultMiddleware} from '@reduxjs/toolkit'
 import cartReducer from './features/cartSlice'
 import filterReducer from './features/filterSlice'
+import logger from 'redux-logger'
 
 const store = configureStore({
     reducer : {
         cart : cartReducer,
         filter : filterReducer
-    }
+    },
+    middleware:(getDefaultMiddleware)=>getDefaultMiddleware().concat(logger)
 })
 
 export default store
